2013e8900a4fd87285f82182fa50d8e6753d9c67
lirongqi <lirongqi@zhenghe.cn>
2025-06-02 16:27:09 +0800

    add 国际化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div id="app">
-    <BasicLayout/>
-  </div>
+  <a-config-provider :locale="locale">
+    <div id="app">
+      <BasicLayout/>
+    </div>
+  </a-config-provider>
 </template>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
 import {useLoginUserStore} from "@/stores/counter";
-const loginUserStore = useLoginUserStore()
-loginUserStore.fetchLoginUser()
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import {onMounted, ref} from 'vue';
+
+// 定义响应式数据
+const locale = ref(zhCN);
+
+// 创建 store 实例
+const loginUserStore = useLoginUserStore();
+
+// 在组件挂载后获取用户信息
+onMounted(() => {
+  loginUserStore.fetchLoginUser();
+});
 
 </script>
 
